06d0f6115a69a8bc360f1fa0b626143275647df2
Amir Ghasemi <83651115+Amirgh8080@users.noreply.github.com>
2023-05-23 19:35:16 +0330

    Teacher

--- src/Modules/Core/CoreModule.Domain/Teacher/Models/Teacher.cs
+++ src/Modules/Core/CoreModule.Domain/Teacher/Models/Teacher.cs
@@ -3,10 +3,18 @@ using CoreModule.Domain.Teacher.Enunms;
 
 namespace CoreModule.Domain.Teacher.Models;
 
-public class Teacher:AggregateRoot
+public class Teacher : AggregateRoot
 {
-    public Guid UserId { get; set; }
-    public string UserName { get; set; }
-    public string CvFileName{ get; set; }
-    public TeacherStatus Status { get; set; }
+    public Teacher(Guid userId, string userName, string cvFileName, TeacherStatus status)
+    {
+        UserId = userId;
+        UserName = userName;
+        CvFileName = cvFileName;
+        Status = status;
+    }
+
+    public Guid UserId { get; private set; }
+    public string UserName { get; private set; }
+    public string CvFileName { get; private set; }
+    public TeacherStatus Status { get; private set; }
 }
