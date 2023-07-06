527eb0f1d69b5354ea7e66108eccd8b91c470e73
Amir Ghasemi <83651115+Amirgh8080@users.noreply.github.com>
2023-07-06 16:29:22 +0330

    Add Course Teacher

--- src/EndPoints/DigiLearn.Web/Controllers/AjaxController.cs
+++ src/EndPoints/DigiLearn.Web/Controllers/AjaxController.cs
@@ -1,4 +1,5 @@
-﻿using CoreModule.Facade.Category;
+﻿using Common.Application.FileUtil.Interfaces;
+using CoreModule.Facade.Category;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DigiLearn.Web.Controllers;
@@ -6,10 +7,12 @@ namespace DigiLearn.Web.Controllers;
 public class AjaxController : Controller
 {
     private readonly ICourseCategoryFacade _categoryFacade;
+    private readonly ILocalFileService _localFileService;
 
-    public AjaxController(ICourseCategoryFacade categoryFacade)
+    public AjaxController(ICourseCategoryFacade categoryFacade, ILocalFileService localFileService)
     {
         _categoryFacade = categoryFacade;
+        _localFileService = localFileService;
     }
 
     [Route("/ajax/getCategoryChildren")]
@@ -22,5 +25,18 @@ public class AjaxController : Controller
             text += $"<option value='{item.Id}'>{item.Title}</option>";
         }
         return new ObjectResult(text);
+    }
+
+    [Route("/Upload/ImageUploader")]
+    public async Task<IActionResult> UploadImage(IFormFile upload)
+    {
+        if (upload == null)
+            return null;
+
+        var fileName = await _localFileService.SaveFileAndGenerateName(upload, "wwwroot/images/upload");
+
+        string url = $"/images/upload/{fileName}";
+
+        return Json(new { Uploaded = true, url });
     }
 }
